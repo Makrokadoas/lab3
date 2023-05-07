b0660486237a89ea0cd15312dcc813309fc6f6c3
Makrokadoas <114870810+Makrokadoas@users.noreply.github.com>
2023-05-07 20:41:47 +0300

    Update script_lab3.sh

--- script_lab3.sh
+++ script_lab3.sh
@@ -5,10 +5,10 @@ optimizer=( "-xSSE2" "-xSSE3" "-xAVX"  )
 for optim in "${optimizer[@]}"; do
   for label in {1..3}; do
     filename1=$(date +%M)
-    filename12=$(date +%S)
+    filename2=$(date +%S)
     echo icpc -std=c++17 -O$label $optim  lab3_tsybulskyi.cpp -o go-$filename1-$filename2
     icpc -std=c++17 -O$label $optim  lab3_tsybulskyi.cpp -o go-$filename1-$filename2
     echo "$optim  $label:"
-    time ./go-$filename11-$filename12
+    time ./go-$filename1-$filename2
   done 
 done
